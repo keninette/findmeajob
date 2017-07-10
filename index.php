dd83c81de18e2e526c0c7bc45a15ed788f53575e
kbj@junior <keninette.bloggeusedeluxe@gmail.com>
2017-07-10 14:16:30 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE>
+<head>
+    <!-- CDN -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- custom css -->
+    <link rel="stylesheet" href="css/main.css" />
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-xs-2">
+                <ul class="nav nav-sidebar navbar-inverse">
+                    <li class="active">plop</li>
+                    <li>plup</li>
+                </ul>
+            </nav>
+            <section class="col-xs-10">
+                <input class="btn btn-lg btn-info" value="plopiplop"/>
+            </section>
+        </div>
+    </div>
+    
+    <!-- scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</body>
