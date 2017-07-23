d12a5832e86c5b11dc0763a80d99c5aaba383edc
kbj@junior <keninette.bloggeusedeluxe@gmail.com>
2017-07-23 20:36:38 +0200

    history application-forms created, jquery to fill and display coded and working. Need to code controller for submit buttons

--- public/js/history.js
+++ public/js/history.js
@@ -1,7 +1,53 @@
 // To be executed as soon as the whole DOM has been loaded
 $(function(){
     
-    $("tr:not(.heading)").hover(function(){
-        $(this).toggleClass("table-hover");
+    // Every time the mouse comes over a table line
+    // We add a css class on it to make it look like it's selected
+    // And remove it when mouse goes out
+    $("tr:not(.heading)")
+        .mouseover(function(){
+            $(this).addClass("table-hover");
+        })
+        .mouseout(function(){
+            $(this).removeClass("table-hover")    ;
+        });
+    
+    // Every time we click on a table line
+    // We display forms to update or re-send application
+    // And we prefill it
+    $("tr:not(.heading)").click(function(){
+        
+        // Get application id from the class of the line that's been selected
+        var id      = $(this).attr("id");
+        
+        // This class should look like "tr-" and followed by application ID
+        // But this could change, so instead of looking for "tr-" in the string
+        // We'll look for the last iteration of "-" position, to its end
+        // This way, if we decide to change <tr> elements ID, we'll still have the right value
+        // That is, of course, if we still end it with "-" +application ID
+        var idValue = id.substring(id.lastIndexOf("-") +1, id.length);
+        
+        // Now that we've got application ID
+        // We can look for the columns in table and be sure to be on the right table index
+        // And get this line content
+        var salutationValue     = $("#salutation-" +idValue).text();
+        var motivationValue     = $("#motivation-" +idValue).text();
+        var answerDateValue     = $("#answer_date-" +idValue).text();
+        var meetingDateValue    = $("#meeting_date-" +idValue).text();
+        var emailValue          = $("#email-" +idValue).text();
+       
+        // Now we've got all the content we need
+        // We fill the application-forms
+        $("#form-id-update").attr("value", idValue);
+        $("#form-id-resend").attr("value", idValue);
+        $("#form-email").attr("value", emailValue);
+        $("#form-answer_date").attr("value", answerDateValue);
+        $("#form-meeting_date").attr("value", meetingDateValue);
+        $("#form-salutation").attr("value", salutationValue);
+        $("#form-motivation").html(motivationValue);
+        
+        // Show application forms, prefilled for user        
+        $("#application-forms").removeClass("hidden");
+        
     });
 });
--- view/history.view.php
+++ view/history.view.php
@@ -1,3 +1,5 @@
+<!-- table with all aplications -->
+<section class="row medium-margin-left">
 <?php
 
 // We only display table if there are some applications to display
@@ -10,7 +12,8 @@ if (isset($displayTable) && $displayTable) {
 //                    , company
 //                    , customized_motivation
 ?>    
-    <table class="table table-stripped">
+    <p class="small-info row">Sélectionnez une candidature pour la mettre à jour ou l'envoyer de nouveau.</p>
+    <table class="table table-stripped row">
         <tr class="heading">
             <th class="hidden">Identifiant</th>
             <th class="hidden">Formule de salutation</th>
@@ -33,12 +36,12 @@ if (isset($displayTable) && $displayTable) {
     //      that's good practice, and that's way easier to maintain
     foreach($applications as $application) {
 ?>
-        <tr>
-            <td class="hidden" id="id-<?php echo $application['id']; ?>"><?php echo $application['id'] ?></td>
-            <td class="hidden" id="salutation-<?php echo $application['id']; ?>"><?php echo $application['salutation'] ?></td>
-            <td class="hidden" id="motivation-<?php echo $application['id']; ?>"><?php echo $application['customized-motivation'] ?></td>
-            <td id="company-<?php echo $application['id']; ?>"><?php echo $application['company'] ?></td>
-            <td id="email-<?php echo $application['id']; ?>"><?php echo $application['email'] ?></td>
+        <tr id="tr-<?php echo $application['id']; ?>">
+            <td class="hidden" id="id-<?php echo $application['id']; ?>"><?php echo $application['id']; ?></td>
+            <td class="hidden" id="salutation-<?php echo $application['id']; ?>"><?php echo $application['salutation']; ?></td>
+            <td class="hidden" id="motivation-<?php echo $application['id']; ?>"><?php echo $application['customized_motivation']; ?></td>
+            <td id="company-<?php echo $application['id']; ?>"><?php echo $application['company']; ?></td>
+            <td id="email-<?php echo $application['id']; ?>"><?php echo $application['email']; ?></td>
             <td id="first_sent_date-<?php echo $application['id']; ?>"><?php echo setDateFormat($application['first_sent_date']); ?></td>
             <td id="last_sent_date-<?php echo $application['id']; ?>"><?php echo setDateFormat($application['last_sent_date']); ?></td> 
             <td id="answer_date-<?php echo $application['id']; ?>"><?php echo setDateFormat($application['answer_date']); ?></td> 
@@ -50,3 +53,30 @@ if (isset($displayTable) && $displayTable) {
     </table>
 <?php
 }
+?>
+</section>
+
+<!-- form to update selected application, hidden at loadind. Made visible and pre-filled when a line of table is selected -->
+<section id="application-forms" class="hidden row medium-margin-left">
+    <form action="index.php?page=history&target=update" method="POST" class="row">
+        <h2>Mettre à jour la candidature</h2>
+        <!-- we put a hidden input to fill it with application ID : the user won't see it but it still will be in $_POST -->
+        <input type="hidden" name ="application[id-update]" id="form-id-update" />
+        <label for="application[answer_date]" class="col-xs-2">Date de réponse</label>
+        <input type="date" id="form-answer_date" name="application[answer_date]" class="col-xs-2" />
+        <label for="application[meeting_date]" class="col-xs-offset-1 col-xs-2">Date d'entretien</label>
+        <input type="date" id="form-meeting_date" name="application[meeting_date]" class="col-xs-2"/>
+        <input type="submit" value="Enregistrer" class="btn btn-default col-xs-1 col-xs-offset-2"/>
+    </form>
+    <form action="index.php?page=history&target=resend" method="POST" class="row">
+        <h2>Renvoyer la candidature</h2>
+        <!-- we put a hidden input to fill it with application ID : the user won't see it but it still will be in $_POST -->
+        <input type="hidden" name ="application[id-resend]" id="form-id-resend" class="col-xs-12"/>
+        <input type="hidden" name ="application[email]" id="form-email" class="col-xs-12"/>
+        <label for="application[salutation]" class="col-xs-12">Salutation</label>
+        <input type="text" id="form-salutation" name="application[salutation]" class="col-xs-12" />
+        <label for="application[motivation]">Personnalisation de la lettre de motivation</label>
+        <textarea name="application[motivation]" id="form-motivation" class="col-xs-12" rows="10"></textarea>
+        <input type="submit" value="Envoyer" class="btn btn-default col-xs-offset-11 col-xs-1 small-margin-top"/>
+    </form>  
+</section>
